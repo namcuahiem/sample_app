a1d38062d06f2eea9d589ef594e3b144388ca7fd
namcuahiem <namcuahiem@gmail.com>
2014-07-27 04:01:08 +0700

    finish add user model with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum:15}, uniqueness: {case_sensitive: false}
 
-	VALID_EMAIL_REGEX = /[\w\.\+-]+@[[a-z]\.\d-]+[a-z]\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w\.\+-]+@[a-z\.\d-]+[a-z\d]\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
 	
 	has_secure_password #method chiu trach nhiem ve tao cac thuoc tinh ao password va password_confirmation
 	
-	validates :password, length: {minimum: 6} #presence da duoc khai bao trong method has_secure_password
+	validates :password, presence: true, length: {minimum: 6}
+	validates :password_confirmation, presence: true
 
 end
