6d7130dd46bc873852b7185ab839bcf4fd5d2522
namcuahiem <namcuahiem@gmail.com>
2014-07-30 09:10:12 +0700

    finish follower and newsfeed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index,:update,:edit,:destroy]
+  before_action :signed_in_user, only: [:index,:update,:edit,:destroy, :following, :followers]
   before_action :correct_user, only: [:update,:edit]
   before_action :admin_user, only: [:destroy]
 
@@ -48,6 +48,21 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
   	def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+
+	has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
+
+	validates :follower_id, presence: true
+	validates :followed_id, presence: true
 
 	#before_save { |user| user.email = user.email.downcase }
 	before_save { self.email = email.downcase }
@@ -23,8 +32,21 @@ class User < ActiveRecord::Base
 	end
 
 	def feed
-	# This is preliminary. See "Following users" for the full implementation.
-		Micropost.where("user_id = ?", id)
+		# This is preliminary. See "Following users" for the full implementation.
+		#Micropost.where("user_id = ?", id)
+		Micropost.from_users_followed_by(self)
+	end
+
+	def following?(someone)
+		self.relationships.find_by(followed_id: someone.id)
+	end
+
+	def follow!(someone)
+		self.relationships.create!(followed_id: someone.id)
+	end
+
+	def unfollow!(someone)
+		self.relationships.find_by(followed_id: someone.id).destroy
 	end
 
 	private
